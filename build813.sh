119842e5bb0cb816936d158327fcd42c83004f4d
dingyanxu <dingyanxu@dreame.tech>
2023-08-21 14:38:36 +0800

    modify build813.sh

--- build813.sh
+++ build813.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-SYSTEM_PATH=~/Project/Mower/mower-ros/system
-IMG_PATH=~/Project/Mower/mower-ros/system/build/output/images/version
+SYSTEM_PATH=~/project/p2255/mower-ros/system
+IMG_PATH=~/project/p2255/mower-ros/system/firmware
 PHOENIXSUIT_PATH=~/Bin/phoenixsuit
 
 cd $SYSTEM_PATH && ./mkfirmware.sh
 IMG="$(cd $IMG_PATH && ls | grep "factory" | sed '/sha256sum/d')"
 cp $IMG_PATH/$IMG $PHOENIXSUIT_PATH
 cd $PHOENIXSUIT_PATH
+rm -rf *.img
 sudo ./phoenixsuit $IMG && rm -rf $IMG

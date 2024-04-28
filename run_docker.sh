bcf093a8f0e16b1339af84dfbdf8131a603b4207
dingyanxu <dingyanxu@dreame.tech>
2024-04-28 14:28:47 +0800

    fix run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-sudo docker run -it -u builder -v $PWD:/home/builder/work  -w /home/builder/work  harbor.dreame.tech/catkin-arm/rbuild:v7
+docker run -it -u builder -v $PWD:/home/builder/work  -w /home/builder/work  harbor.dreame.tech/catkin-arm/rbuild:v7
